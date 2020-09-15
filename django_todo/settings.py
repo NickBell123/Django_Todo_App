79fafb0e680fd6e2590d9435f7ba42ca8bbbcfaa
NickBell123 <nick.bell2008@gmail.com>
2020-09-15 11:07:48 +0100

    deleted old git repo, updated gitignore to include database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://rnknzltyfpwkpi:56e5f7f652b40ac3eefa76a3fab9b348979742012c5f6898b5a464aec3cafef7@ec2-34-251-118-151.eu-west-1.compute.amazonaws.com:5432/dea6a2odsoum1k', conn_max_age=600)
 }
 
 
